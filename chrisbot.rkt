1182b7ed0e0f5857649acd4fbec95bd7895b1ab2
Chrs Matzenbach <cmatzenbach@gmail.com>
2017-06-06 16:18:28 -0400

    Initial commit

---
+++ chrisbot.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+; Use the random function implemented in Racket
+; (random k) returns a random integer in the range 0 to k-1
+(#%require (only racket/base random))
+
+; I/O Helper Functions
+
+; Nil -> List
+; Prompt user for input
+(define (prompt)
+  (newline)
+  (display "talk to me >>>")
+  (read-user-line))
+
+; Nil -> List
+; Read user input until end of character
+(define (read-user-line)
+  (let (next(read))
+    (if (eof-object? next)
+        '()
+        (cons next (read-user-line)))))
+
+; List -> Nil
+; Display output from list of chars
+(define (output lst)
+  )
