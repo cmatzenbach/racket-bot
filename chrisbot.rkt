64d3d43d5ba0eb954eb54311b30e39a69a20ef59
cmatzenbach <cmatzenbach@gmail.com>
2017-06-07 01:24:42 -0400

    Finished main func and chat loop, need to implement dependencies

--- chrisbot.rkt
+++ chrisbot.rkt
@@ -1,7 +1,10 @@
-#lang racket
 ; http://www.chegg.com/homework-help/questions-and-answers/task-implement-simple-chatbot-racket--s-sample-chat-program-carry-chat-name-hi-name-talk-a-q20637753
 
+; CHRIS-BOT
+#lang racket
 
+; module containing to-string
+;; (require web-server/formlets/input)
 ; Use the random function implemented in Racket
 ; (random k) returns a random integer in the range 0 to k-1
 (#%require (only racket/base random))
@@ -18,7 +21,7 @@
 ; Nil -> List
 ; Read user input until end of character
 (define (read-user-line)
-  (let (next(read))
+  (let ((next(read)))
     (if (eof-object? next)
         '()
         (cons next (read-user-line)))))
@@ -26,4 +29,35 @@
 ; List -> Nil
 ; Display output from list of chars
 (define (output lst)
-  )
+  (newline)
+  (display (to-string lst))
+  (newline))
+
+(define (to-string lst)
+  (cond
+    [(empty? lst) ""]
+    [(eq? (length lst) 1) (symbol->string(car lst))]
+    [else (string-append(symbol->string(car lst)))]
+    ))
+
+; Main Function
+; usage: (chat-with 'your-name)
+(define (chat-with name)
+  (output (list 'hi name))
+  (chat-loop name))
+
+; chat loop
+(define (chat-loop name)
+  (let ((input(prompt)))   ; get user input
+    (if (eqv? (car input) 'bye)
+        (begin
+          (output (list 'bye name))
+          (output(list 'have 'a 'great 'day!)))
+        (begin
+          (reply input name)
+          (chat-loop name)))))
+
+(define (reply input name)
+  (output 'Hello 'there))
+
+(chat-with 'Chris)
